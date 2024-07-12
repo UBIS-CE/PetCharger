e581fb76bb2bf24d5a163eb62035d41deaadb032
lch010201 <lch010201@gmail.com>
2024-07-12 15:47:08 +0900

    feat: 동물 정보 및 진료기록 삭제 구현

--- step04_miniProject/src/ubis/service/PetChargerService.java
+++ step04_miniProject/src/ubis/service/PetChargerService.java
@@ -22,20 +22,13 @@ public class PetChargerService {
 	public static PetChargerService getInstance() {
 		return instance;
 	}
+	
 	public void animalInsert(Animal animal) throws Exception {
 		Animal a = getAnimal(animal.getPetId());
 		if (a != null) {
 			throw new Exception("해당 animal Id는이미 존재합니다. 재 확인하세요");
 		}
 		animalList.add(a);
-	}
-	private Animal getAnimal(Integer petId) {
-		for (Animal animal : animalList) {
-			if (animal != null && animal.getPetId().equals(petId)) {
-				return animal; //메소드 자체의 종료
-			}
-		}
-		return null;
 	}
 		
 	public ArrayList<Animal> getAnimalList(String petName) {
@@ -44,9 +37,19 @@ public class PetChargerService {
 	public ArrayList<MedicalRecord> getMedicalRecordList(int animalPK) {
 		return null;
 	}
+	
+	/**
+	 * animalPK를 animal list와 mediclist에서 삭제
+	 * @param animalPK
+	 */
 	public void animalAndMedicalRecordDelete(int animalPK) {
-		
+		Animal animal = animalList.get(animalPK);
+		if(animal != null) {
+			animalList.remove(animalPK);
+			medicalRecordList.remove(animalPK);
+		}
 	}
+	
 	public void medicalRecordInsert(MedicalRecord medicalRecord) throws Exception {
 		MedicalRecord m = getMedicalRecord(medicalRecord.getPk());
 		if (m != null) {
@@ -54,6 +57,16 @@ public class PetChargerService {
 		}
 		medicalRecordList.add(m);
 	}
+	
+	private Animal getAnimal(Integer petId) {
+		for (Animal animal : animalList) {
+			if (animal != null && animal.getPetId().equals(petId)) {
+				return animal; //메소드 자체의 종료
+			}
+		}
+		return null;
+	}
+	
 	private MedicalRecord getMedicalRecord(int pk) {
 		for (MedicalRecord medical : medicalRecordList) {
 			if (medical != null && medical.getPk()==pk) {
