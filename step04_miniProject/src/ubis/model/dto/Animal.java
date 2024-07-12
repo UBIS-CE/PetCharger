d5c25930e7d0129a15cd4bc9a46fc7f652fd180b
dndqlszzz <mandarin1999@naver.com>
2024-07-12 12:44:13 +0900

    Add Animal.java

---
+++ step04_miniProject/src/ubis/model/dto/Animal.java
@@ -0,0 +1,16 @@
+package ubis.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter  
+@Setter
+
+public class Animal {
+	private String pet_id;
+	
+}
