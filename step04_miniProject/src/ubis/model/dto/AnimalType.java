f64959de0c5efa8eea8bba9bdd445f3da4fa97bf
lch010201 <lch010201@gmail.com>
2024-07-12 12:04:30 +0900

    feat: 동물종류, 암수구분 enum 클래스

---
+++ step04_miniProject/src/ubis/model/dto/AnimalType.java
@@ -0,0 +1,9 @@
+package ubis.model.dto;
+
+public enum AnimalType {
+	DOG,
+	CAT,
+	FERRET, //패럿
+	HEDGEHOG, //고슴도치
+	PARROT; //앵무새
+}
---
+++ step04_miniProject/src/ubis/model/dto/Gender.java
@@ -0,0 +1,8 @@
+package ubis.model.dto;
+
+public enum Gender {
+	F, //암컷
+	M; //수컷
+	
+}
+    
